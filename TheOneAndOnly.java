2ec06fcfd017cd857e45473c2ee908bdbf4e33cd
kinw-oss <59590246+kinw-oss@users.noreply.github.com>
2021-02-01 21:26:06 -0500

    Update TheOneAndOnly.java

--- TheOneAndOnly.java
+++ TheOneAndOnly.java
@@ -17,23 +17,26 @@ public class TheOneAndOnly extends LinearOpMode {
     private DcMotor backLeft;
     private DcMotor firstLauncher;
     private DcMotor secondLauncher;
+    private DcMotor wobbler;
     private Servo lifter;
     private Servo shifter;
+    private Servo grabber;
 
-    private double shooterPower = 0.5;
+    private double shooterPower = 0.45;
 
     @Override
     public void runOpMode() {
 
         waitForStart();
 
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backRight  = hardwareMap.get(DcMotor.class, "backRight");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
 
-        firstLauncher = hardwareMap.get(DcMotor.class, "firstLauncher");
+        firstLauncher  = hardwareMap.get(DcMotor.class, "firstLauncher");
         secondLauncher = hardwareMap.get(DcMotor.class, "secondLauncher");
+        wobbler        = hardwareMap.get(DcMotor.class, "wobbler");
 
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -43,70 +46,98 @@ public class TheOneAndOnly extends LinearOpMode {
         secondLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
-        lifter = hardwareMap.get(Servo.class, "lifter");
+        lifter  = hardwareMap.get(Servo.class, "lifter");
         shifter = hardwareMap.get(Servo.class, "shifter");
+        grabber = hardwareMap.get(Servo.class, "grabber");
 
         shifter.setPosition(1);
+        grabber.setPosition(0.9);
 
         while (opModeIsActive()) {
 
-            //shitty way of assigning power
+            double strafePower = 1;
             double rightPower = -gamepad1.right_stick_y;
             double leftPower = gamepad1.left_stick_y;
+            double wobblerPower = gamepad2.left_stick_x/1.5;
 
             backRight.setPower(rightPower);
-            frontRight.setPower(rightPower);
             backLeft.setPower(leftPower);
+            frontRight.setPower(rightPower);
             frontLeft.setPower(leftPower);
 
-            //charges shooter
-            if (gamepad1.left_trigger > 0) {
+            wobbler.setPower(wobblerPower);
+
+            //strafe left
+            if (gamepad1.dpad_left) {
+                backRight.setPower(-strafePower);
+                frontRight.setPower(strafePower);
+                backLeft.setPower(strafePower);
+                frontLeft.setPower(-strafePower);
+            }
+
+            //strafe right
+            if (gamepad1.dpad_right) {
+                backRight.setPower(strafePower);
+                frontRight.setPower(-strafePower);
+                backLeft.setPower(-strafePower);
+                frontLeft.setPower(strafePower);
+            }
+
+            //start shooter motors
+            if (gamepad2.left_trigger > 0) {
                 setShooterPower(shooterPower);
-            } else if (gamepad1.left_trigger == 0) {
+            } else {
                 setShooterPower(0);
             }
 
-            //shoots one ring...shooter must be charged first with left trigger
-            if (gamepad1.a) {
+            //shoot 1 ring
+            if (gamepad2.a) {
                 shifter.setPosition(0.57);
                 sleep(500);
                 shifter.setPosition(1);
             }
 
-            //increments power of shooter up
-            if (gamepad1.dpad_up) {
+            //iterates shooterPower up 0.01
+            if (gamepad2.dpad_up) {
                 shooterPower += 0.01;
-                sleep(200);
+                sleep(100);
             }
-                                                                            
-            //increments power of shooter down
-            if (gamepad1.dpad_down) {
+
+            //iterates shooterPower down 0.01
+            if (gamepad2.dpad_down) {
                 shooterPower -= 0.01;
-                sleep(200);
-            }
-            //moves lifter down incrementally, so it doesn't jerk down
-            if (gamepad1.x) {
-                double position = 0.87;
-
-                lifter.setPosition(position);
-
-                for (int i=0; i<10; i++) {
-                    position -= 0.02;
-                    lifter.setPosition(position);
-                }
+                sleep(100);
             }
 
-            //moves lifter up
-            if (gamepad1.y) {
-                lifter.setPosition(0.87);
+            //smoothly moves lifter down to retrieval position
+            if (gamepad2.x) {
+                lifter.setPosition(0.5);
             }
+
+            //moves lifter up to shooting position
+            if (gamepad2.y) {
+                lifter.setPosition(0.86);
+            }
+
+            if (gamepad2.right_bumper) {
+                grabber.setPosition(grabber.getPosition() + 0.01);
+            }
+            if (gamepad2.left_bumper) {
+                grabber.setPosition(grabber.getPosition() - 0.01);
+            }
+
+
 
             telemetry.addData("shooterPower", shooterPower);
+            telemetry.addData("Right Power", rightPower);
+            telemetry.addData("Left Power", leftPower);
+            telemetry.addData("Grabber Position", grabber.getPosition());
             telemetry.update();
+
         }
     }
 
-    //stupid, unnessecary method. 
+    //idk why i have this. utterly useless
     private void setShooterPower(double power) {
         firstLauncher.setPower(power);
         secondLauncher.setPower(power);
